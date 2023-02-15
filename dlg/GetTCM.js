6263f0e630d0eebda82212eaa2d120f917f49aff
nicolasances <nicolas.matteazzi@gmail.com>
2023-02-15 11:38:56 +0100

    TCM level 1

--- dlg/GetTCM.js
+++ dlg/GetTCM.js
@@ -1,6 +1,16 @@
 
 
 exports.do = async (req, userContext, execContext) => {
-    
-    return {tcm: "Not available for now"}
+
+    return [
+        { code: "interfaces", name: "Interfaces & Mobility" },
+        { code: "iot", name: "IoT" },
+        { code: "integration", name: "Integration" },
+        { code: "analytics", name: "Analytics" },
+        { code: "compute", name: "Compute" },
+        { code: "storage", name: "Storage" },
+        { code: "productivity", name: "Productivity" },
+        { code: "security", name: "Security & Identity" },
+        { code: "network", name: "Networking" }
+    ]
 } 
